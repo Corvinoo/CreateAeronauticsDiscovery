b1428e51fc4026ea0b3e170971ea6f6919e3889f
Leonardo Importuna <39531150+bitmochibit@users.noreply.github.com>
2026-06-23 23:16:47 +0200

    fix(entity-removal-race-conditions) fixed random race conditions that caused the flyovers to spawn without parts entities, or not assembling entirely.
    Signed-off-by: Leonardo Importuna <39531150+bitmochibit@users.noreply.github.com>

--- src/main/java/me/corvino/aeronauticsdiscovery/assembly/steps/PlaceBlocksStep.java
+++ src/main/java/me/corvino/aeronauticsdiscovery/assembly/steps/PlaceBlocksStep.java
@@ -1,12 +1,20 @@
 package me.corvino.aeronauticsdiscovery.assembly.steps;
 
+import me.corvino.aeronauticsdiscovery.CreateAeronauticsDiscovery;
 import me.corvino.aeronauticsdiscovery.assembly.AssemblyContext;
 import me.corvino.aeronauticsdiscovery.assembly.AssemblyResult;
 import me.corvino.aeronauticsdiscovery.assembly.AssemblyStep;
 import me.corvino.aeronauticsdiscovery.seat.SeatPopulator;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
+import net.minecraft.world.phys.AABB;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlaceBlocksStep implements AssemblyStep {
     @Override
@@ -14,6 +22,9 @@ public class PlaceBlocksStep implements AssemblyStep {
         if (ctx.assemblerPos != null) {
             return AssemblyResult.SUCCESS;
         }
+        assert ctx.template != null;
+        assert ctx.anchor != null;
+        assert ctx.level != null;
 
         Rotation rot = ctx.rotationTemplate != null ? ctx.rotationTemplate : Rotation.NONE;
         StructurePlaceSettings settings = new StructurePlaceSettings()
@@ -32,9 +43,34 @@ public class PlaceBlocksStep implements AssemblyStep {
             return AssemblyResult.FAIL;
         }
 
+//        debugEntitiesInArea(ctx.level, AABB.of(ctx.bounds));
+
         ctx.worldSeatPositions.clear();
         ctx.worldSeatPositions.addAll(SeatPopulator.findSeatPositions(ctx.level, ctx.bounds));
 
         return AssemblyResult.SUCCESS;
+    }
+
+    private void debugEntitiesInArea(ServerLevel level, AABB bounds) {
+        AABB expandedBounds = bounds.inflate(1.0);
+        var allEntities = level.getAllEntities();
+        var entityCount = 0;
+
+        CreateAeronauticsDiscovery.LOGGER.info("Flyover entity check start");
+        for (Entity entity : allEntities) {
+            if (expandedBounds.contains(entity.getX(), entity.getY(), entity.getZ())) {
+                CreateAeronauticsDiscovery.LOGGER.info("Found entity: {}", entity);
+                entityCount++;
+            }
+        }
+        if (entityCount > 8) {
+            CreateAeronauticsDiscovery.LOGGER.warn("Found MORE entities than what the default structure contains! Please check!");
+        }
+
+        if (entityCount < 8) {
+            CreateAeronauticsDiscovery.LOGGER.warn("Found LESS entities than what the default structure contains! Please check!");
+        }
+
+        CreateAeronauticsDiscovery.LOGGER.info("Flyover entity check stop");
     }
 }
