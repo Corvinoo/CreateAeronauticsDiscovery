10e2205fed900ee0912c81fba235f87ad5de4264
Corvinoo <39708435+Corvinoo@users.noreply.github.com>
2026-06-25 17:25:52 +0200

    added failrandomly

--- src/main/java/me/corvino/aeronauticsdiscovery/assembly/AssemblyStep.java
+++ src/main/java/me/corvino/aeronauticsdiscovery/assembly/AssemblyStep.java
@@ -1,7 +1,18 @@
 package me.corvino.aeronauticsdiscovery.assembly;
 
+import org.jetbrains.annotations.TestOnly;
+
+import java.util.Random;
+
 public interface AssemblyStep {
     AssemblyResult run(AssemblyContext ctx);
 
     default void cleanup(AssemblyContext ctx) {}
+    
+    @TestOnly
+    static AssemblyResult failRandomly(){
+        var random = new Random().nextInt(3);
+        if(random < 2) return AssemblyResult.FAIL;
+        return AssemblyResult.SUCCESS;        
+    }
 }
