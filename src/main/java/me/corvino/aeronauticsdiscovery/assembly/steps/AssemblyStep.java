7b3b850ff0c0af5bcf62b34504dba9221cc29537
Leonardo Importuna <39531150+bitmochibit@users.noreply.github.com>
2026-06-25 19:32:44 +0200

    fix(spawning-and-manager) removed computer craft and adjusted wait times
    Signed-off-by: Leonardo Importuna <39531150+bitmochibit@users.noreply.github.com>

--- src/main/java/me/corvino/aeronauticsdiscovery/assembly/steps/AssemblyStep.java
+++ src/main/java/me/corvino/aeronauticsdiscovery/assembly/steps/AssemblyStep.java
@@ -2,8 +2,19 @@ package me.corvino.aeronauticsdiscovery.assembly.steps;
 
 import me.corvino.aeronauticsdiscovery.assembly.AssemblyContext;
 import me.corvino.aeronauticsdiscovery.assembly.AssemblyResult;
+import org.jetbrains.annotations.TestOnly;
+
+import java.util.Random;
 
 public interface AssemblyStep {
     AssemblyResult run(AssemblyContext ctx);
+
     default void cleanup(AssemblyContext ctx) {}
+
+    @TestOnly
+    static AssemblyResult failRandomly(){
+        var random = new Random().nextInt(3);
+        if(random < 2) return AssemblyResult.FAIL;
+        return AssemblyResult.SUCCESS;
+    }
 }
