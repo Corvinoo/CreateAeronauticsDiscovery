fcc586f2be889c7e086baee3ce042bbe0628bbb0
Corvinoo <39708435+Corvinoo@users.noreply.github.com>
2026-06-25 15:52:17 +0200

    adding cleanup

--- src/main/java/me/corvino/aeronauticsdiscovery/assembly/steps/PopulateSeatsStep.java
+++ src/main/java/me/corvino/aeronauticsdiscovery/assembly/steps/PopulateSeatsStep.java
@@ -1,8 +1,11 @@
 package me.corvino.aeronauticsdiscovery.assembly.steps;
 
+import dev.ryanhcode.sable.sublevel.ServerSubLevel;
 import me.corvino.aeronauticsdiscovery.assembly.AssemblyContext;
 import me.corvino.aeronauticsdiscovery.assembly.AssemblyResult;
 import me.corvino.aeronauticsdiscovery.assembly.AssemblyStep;
+import me.corvino.aeronauticsdiscovery.entities.SoaringTrader;
+import me.corvino.aeronauticsdiscovery.event.FlyoverManager;
 import me.corvino.aeronauticsdiscovery.seat.SeatPopulator;
 
 public class PopulateSeatsStep implements AssemblyStep {
@@ -16,4 +19,15 @@ public class PopulateSeatsStep implements AssemblyStep {
         ctx.seatsPopulated = true;
         return AssemblyResult.SUCCESS;
     }
+
+
+    @Override
+    public void cleanup(AssemblyContext ctx) {
+        if (ctx.assemblyResult == null) return;
+        if (!(ctx.assemblyResult.subLevel() instanceof ServerSubLevel serverSubLevel)) return;
+        FlyoverManager.removeAllEntitiesInSublevel(serverSubLevel, false,
+                e -> e instanceof SoaringTrader);
+        ctx.seatsPopulated = false;
+    }
+
 }
