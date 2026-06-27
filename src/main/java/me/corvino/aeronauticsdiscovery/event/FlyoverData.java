8b32e8fb3d2d52bdfee327aec3519a49530e121d
Corvinoo <39708435+Corvinoo@users.noreply.github.com>
2026-06-27 18:57:06 +0200

    fixing flyover not spawning if too far from world origin

--- src/main/java/me/corvino/aeronauticsdiscovery/event/FlyoverData.java
+++ src/main/java/me/corvino/aeronauticsdiscovery/event/FlyoverData.java
@@ -2,6 +2,7 @@ package me.corvino.aeronauticsdiscovery.event;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.UUIDUtil;
 import net.minecraft.resources.ResourceLocation;
 
@@ -10,15 +11,17 @@ import java.util.UUID;
 public record FlyoverData(
         UUID subLevelId,
         int lifeTicks,
-        ResourceLocation templateId
+        ResourceLocation templateId,
+        BlockPos spawnPos
 ) {
     public static final Codec<FlyoverData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             UUIDUtil.CODEC.fieldOf("sub_level_id").forGetter(FlyoverData::subLevelId),
             Codec.INT.fieldOf("life_ticks").forGetter(FlyoverData::lifeTicks),
-            ResourceLocation.CODEC.fieldOf("template_id").forGetter(FlyoverData::templateId)
+            ResourceLocation.CODEC.fieldOf("template_id").forGetter(FlyoverData::templateId),
+            BlockPos.CODEC.fieldOf("spawn_pos").forGetter(FlyoverData::spawnPos)
     ).apply(instance, FlyoverData::new));
 
     public FlyoverData tick() {
-        return new FlyoverData(this.subLevelId, this.lifeTicks + 1, this.templateId);
+        return new FlyoverData(this.subLevelId, this.lifeTicks + 1, this.templateId, this.spawnPos);
     }
 }
