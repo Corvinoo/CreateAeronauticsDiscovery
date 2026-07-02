f63224422ff05a946e3f70f229b079f162281a4b
Leonardo Importuna <39531150+bitmochibit@users.noreply.github.com>
2026-07-02 21:21:18 +0200

    fix(assembly-steps) invalid conditional check
    Signed-off-by: Leonardo Importuna <39531150+bitmochibit@users.noreply.github.com>

--- src/main/java/me/corvino/aeronauticsdiscovery/assembly/steps/UnloadChunkStep.java
+++ src/main/java/me/corvino/aeronauticsdiscovery/assembly/steps/UnloadChunkStep.java
@@ -10,7 +10,7 @@ import me.corvino.aeronauticsdiscovery.event.manager.FlyoverManager;
 public class UnloadChunkStep extends AssemblyStep {
     @Override
     protected void build(Sequence seq) {
-        seq.require(ctx -> ctx.template != null || ctx.templateSize, "Template or size missing")
+        seq.require(ctx -> ctx.template != null || ctx.templateSize != null, "Template or size missing")
                 .require(ctx -> ctx.level != null, "Level missing")
                 .require(ctx -> ctx.anchor != null, "Anchor missing")
                 .run(this::unloadChunks);
