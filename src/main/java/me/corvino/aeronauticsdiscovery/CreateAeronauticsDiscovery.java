9552d0f5407d6beaa17d950655edb235895b9158
Corvinoo <39708435+Corvinoo@users.noreply.github.com>
2026-07-06 21:38:31 +0200

    added arrow trigger
    Adding a mixin was necessary in order to make the marker hittable - minecraft vanilla for some reason checks if and entity is alive AND pickable in order to be hittable; our marker cannot be pickable.

--- src/main/java/me/corvino/aeronauticsdiscovery/CreateAeronauticsDiscovery.java
+++ src/main/java/me/corvino/aeronauticsdiscovery/CreateAeronauticsDiscovery.java
@@ -7,6 +7,7 @@ import me.corvino.aeronauticsdiscovery.client.renderer.MarkerEntityRenderer;
 import me.corvino.aeronauticsdiscovery.client.renderer.SoaringTraderRenderer;
 import me.corvino.aeronauticsdiscovery.marker.MarkerEntity;
 import me.corvino.aeronauticsdiscovery.marker.MarkerNetwork;
+import me.corvino.aeronauticsdiscovery.marker.trigger.ProjectileImpactTrigger;
 import me.corvino.aeronauticsdiscovery.commands.CleanChildSubLevelsCommand;
 import me.corvino.aeronauticsdiscovery.commands.DebugCommands;
 import me.corvino.aeronauticsdiscovery.commands.MarkerTestCommand;
@@ -97,6 +98,7 @@ public class CreateAeronauticsDiscovery {
         NeoForge.EVENT_BUS.addListener(FlyoverEventScheduler::onLevelTick);
         NeoForge.EVENT_BUS.addListener(FlyoverManager::onLevelTick);
         NeoForge.EVENT_BUS.addListener(MarkerNetwork::onLevelTick);
+        NeoForge.EVENT_BUS.addListener(ProjectileImpactTrigger::onProjectileImpact);
 //        NeoForge.EVENT_BUS.addListener(StructureProximityTracker::onLevelTick); //disabled for now
         NeoForge.EVENT_BUS.addListener(FlyoverCommands::onPlayerTick);
         NeoForge.EVENT_BUS.addListener(PrefabPhysicsRegistry::onAddReloadListeners);
