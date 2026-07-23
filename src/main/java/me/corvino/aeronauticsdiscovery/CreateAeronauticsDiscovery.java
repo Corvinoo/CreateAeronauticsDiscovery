19216ce1e2b268c58870fd591bc7c970b9b131de
Corvinoo <39708435+Corvinoo@users.noreply.github.com>
2026-07-23 15:09:32 +0200

    preventing placing client-side

--- src/main/java/me/corvino/aeronauticsdiscovery/CreateAeronauticsDiscovery.java
+++ src/main/java/me/corvino/aeronauticsdiscovery/CreateAeronauticsDiscovery.java
@@ -112,7 +112,8 @@ public class CreateAeronauticsDiscovery {
         NeoForge.EVENT_BUS.addListener(PrefabPhysicsRegistry::onAddReloadListeners);
         NeoForge.EVENT_BUS.addListener(FlyoverEventRegistry::onAddReloadListeners);
         NeoForge.EVENT_BUS.addListener(BridgeInteractionHandler::onRightClickBlock);
-        NeoForge.EVENT_BUS.addListener(BridgePlankManager::onEntityPlace);
+        NeoForge.EVENT_BUS.addListener(BridgePlankManager::onRightClickBlock);
+//        NeoForge.EVENT_BUS.addListener(BridgePlankManager::onEntityPlace);
         NeoForge.EVENT_BUS.addListener((net.neoforged.neoforge.event.level.LevelEvent.Save event) -> {
             if (event.getLevel() instanceof ServerLevel serverLevel) {
                 BridgePlankManager.cleanupDeadEntries(serverLevel);
